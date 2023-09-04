50af95a83b158cce1a2fb0ac6384fff061b2b263
revathi1235 <revathivenugopal10@gmail.com>
2023-09-04 12:22:43 +0530

    first commit

---
+++ ftocel.js
@@ -0,0 +1,6 @@
+function ftocel(f){
+ let cel;   
+ cel=(f-32)*5/9;
+ console.log("f to cel is" +cel);
+}
+ftocel(2);
---
+++ mtokm.js
@@ -0,0 +1,6 @@
+function mtokm(m){
+    let km;
+    km=m*1.609;
+   console.log("meter to km "+km);
+}
+mtokm(1);
